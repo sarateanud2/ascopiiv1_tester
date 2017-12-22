d3a5280ea3cf6ecbfbc4631b8d2c2c84c1912f06
DSarateanu <sarateanud2@gmail.com>
2017-12-22 16:27:02 +0200

    Update 22.12.2017

--- AsCopii_Test_v1/src/org/ascop/programs/interfaces/medelsimpl/ModelCategoriiArtOptImpl.java
+++ AsCopii_Test_v1/src/org/ascop/programs/interfaces/medelsimpl/ModelCategoriiArtOptImpl.java
@@ -2,7 +2,7 @@ package org.ascop.programs.interfaces.medelsimpl;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
+
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
---
+++ AsCopii_Test_v1/src/org/ascop/programs/interfaces/services/ServiceStatutTab.java
@@ -0,0 +1,11 @@
+package org.ascop.programs.interfaces.services;
+
+import java.util.List;
+
+import org.ascop.programs.enitys.StatutTab;
+
+
+
+public interface ServiceStatutTab {
+	List<StatutTab> getAllStatutTab();
+}
